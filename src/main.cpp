bf8a8dc333accecad87f4c1d0881af5dc5718a48
LCottin <cottin_leonard@icloud.com>
2022-04-07 20:02:16 +0200

    Updated to read database

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,21 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    
+    FILE *fp = fopen("database/database.csv", "r");
+    if (fp == NULL)
+    {
+        cout << "Error opening file" << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    char line[256];
+    unsigned line_number = 0;
+    while (fgets(line, sizeof(line), fp) != NULL)
+    {
+        line_number++;
+    }
+    cout << line << endl;
+    cout << line_number << endl;
+
     return 0;
 }
