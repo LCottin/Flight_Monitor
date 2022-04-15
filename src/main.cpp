d7cd596856456ac251084d2c4fc9c2566154fe75
Debian <cottin_leonard@icloud.com>
2022-04-15 16:40:24 +0200

    Updated test

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,6 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-
     /*
     FILE *fp = fopen("database/database.csv", "r");
     if (fp == NULL)
@@ -23,10 +22,6 @@ int main(int argc, char const *argv[])
 
     char line[256];
     unsigned line_number = 0;
-    unsigned time;
-    char id[6], squawk[4];
-    double lat, lon, velo, heading, velo_ver, baro_alt, gps_alt, last_post, last_contact; 
-    char ground[10], alert[10], spi[10], callsign[10];
 
     while (fgets(line, sizeof(line), fp) != NULL && line_number < 40)
     {
@@ -43,46 +38,9 @@ int main(int argc, char const *argv[])
         {
             cout << "Error in line " << line_number << endl;
         }
-        else
-        {
-            time = atoi(token_array[0]);
-            strcpy(id, token_array[1]);
-            lat = atof(token_array[2]);
-            lon = atof(token_array[3]);
-            velo = atof(token_array[4]);
-            heading = atof(token_array[5]);
-            velo_ver = atof(token_array[6]);
-            strcpy(callsign, token_array[7]);
-            strcpy(ground, token_array[8]);
-            strcpy(alert, token_array[9]);
-            strcpy(spi, token_array[10]);
-            strcpy(squawk, token_array[11]);
-            baro_alt = atof(token_array[12]);
-            gps_alt = atof(token_array[13]);
-            last_post = atof(token_array[14]);
-            last_contact = atof(token_array[15]);
-            cout << "time: " << time << endl;
-            cout << "id: " << id << endl;
-            cout << "lat: " << lat << endl;
-            cout << "lon: " << lon << endl;
-            cout << "velo: " << velo << endl;
-            cout << "heading: " << heading << endl;
-            cout << "velo_ver: " << velo_ver << endl;
-            cout << "callsign: " << callsign << endl;
-            cout << "ground: " << ground << endl;
-            cout << "alert: " << alert << endl;
-            cout << "spi: " << spi << endl;
-            cout << "squawk: " << squawk << endl;
-            cout << "baro_alt: " << baro_alt << endl;
-            cout << "gps_alt: " << gps_alt << endl;
-            cout << "last_post: " << last_post << endl;
-            cout << "last_contact: " << last_contact << endl;
-            cout << endl;
-        }
 
         printf("\n\n");
     }
-    
     fclose(fp);
     */
 
@@ -116,29 +74,11 @@ int main(int argc, char const *argv[])
     cout << "ID: " << plane.getID() << endl;
     cout << "Squawk: " << plane.getSquawk() << endl;
     cout << "Call sign: " << plane.getCallSign() << endl;
-    cout << "Is grounded: " << plane.getIsGrounded() << endl;
-    cout << "Is alerted: " << plane.getIsAlerted() << endl;
-    cout << "Is SPI: " << plane.getIsSPI() << endl;
+    cout << "Is grounded: " << plane.isGrounded() << endl;
+    cout << "Is alerted: " << plane.isAlerted() << endl;
+    cout << "Is SPI: " << plane.isSPI() << endl;
     cout << "Latitude: " << plane.getLatitude() << endl;
     cout << "Longitude: " << plane.getLongitude() << endl;
-
-    printf("\n");
-    //using the geters with printf
-    printf("Ground velocity: %lf\n", plane.getGroundVelocity());
-    printf("Vertical velocity: %lf\n", plane.getVerticalVelocity());
-    printf("GPS altitude: %lf\n", plane.getGPSAltitude());
-    printf("Baro altitude: %lf\n", plane.getBaroAltitude());
-    printf("Heading: %lf\n", plane.getHeading());
-    printf("Last post: %lf\n", plane.getLastPost());
-    printf("Last contact: %lf\n", plane.getLastContact());
-    printf("ID: %s\n", plane.getID().c_str());
-    printf("Squawk: %s\n", plane.getSquawk().c_str());
-    printf("Call sign: %s\n", plane.getCallSign().c_str());
-    printf("Is grounded: %s\n", plane.getIsGrounded() ? "True":"False");
-    printf("Is alerted: %s\n", plane.getIsAlerted() ? "True":"False");
-    printf("Is SPI: %s\n", plane.getIsSPI() ? "True":"False");
-    printf("Latitude: %lf\n", plane.getLatitude());
-    printf("Longitude: %lf\n", plane.getLongitude());
 
     printf("\n");
     plane.printInfo();
