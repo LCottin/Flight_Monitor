b7f5cf97deea9bfc8f9b811e8bd8ed7fea65e9be
Debian <cottin_leonard@icloud.com>
2022-04-29 15:47:03 +0200

    Creatd files with parameters

---
+++ include/PARAM.hpp
@@ -0,0 +1,15 @@
+/**************************
+ *       PARAM FILE       *
+ **************************/
+
+#ifndef __PARAM__
+#define __PARAM__
+
+#define IMG_NAME        "imgs/map.jpg"
+#define IMG_WIDTH       900
+#define IMG_HEIGHT      600
+
+#define PLANE_TEXTURE   "imgs/plane.png"
+#define PLANE_SCALE     0.02
+
+#endif
